3f96c9285a7e8f61b0b8794a45c7f2cb01298b64
Rushil Chopra <83889502+MntRushmore@users.noreply.github.com>
2025-06-19 18:20:30 -0700

    Fix duplicate function and typos

--- script.js
+++ script.js
@@ -39,24 +39,10 @@ changeItem(3, "Quit Game")
 } 
 }
 
-function deselectItem(item) {
-if (item == 1) {
-changeItem(0, "Start Game")
-} 
-if (item == 2) {
-changeItem(1,"How to Play")
-} 
-if (item == 3) {
-changeItem(2, "Rewards")
-} 
-if (item == 4) {
-changeItem(3, "Quit Game")
-} 
-}
 
 function runItem(item) {
 if (item == 1) {
-alert("submisson form not ready yet :(")
+alert("submission form not ready yet :(")
 } 
 if (item == 2) {
 window.location.href = "/howtoplay.html"
